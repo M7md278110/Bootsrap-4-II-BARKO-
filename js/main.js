1211ce67c678d708cef1340573291f04804b1902
Mohamed Hegazy <m.mohamed10012@gmail.com>
2020-10-27 18:16:20 +0200

    Finished

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    // fitText
+    $('h1').fitText(1.1, { minFontSize: '45px', maxFontSize: '75px' })
+    // Create Padding-top For Body
+    //$('body').css('padding-top', $('nav').innerHeight());
+})
